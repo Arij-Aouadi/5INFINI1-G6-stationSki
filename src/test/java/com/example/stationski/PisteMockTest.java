be354c4725c8680f2798138a11be0bf2f7ff59e2
donia532 <donia.aloulou@esprit.tn>
2024-02-27 13:10:00 +0100

    mock2555

--- src/test/java/com/example/stationski/PisteMockTest.java
+++ src/test/java/com/example/stationski/PisteMockTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.example.stationski;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import com.example.stationski.entities.couleur;
+import com.example.stationski.entities.Couleur;
 import com.example.stationski.entities.Piste;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,30 +18,30 @@ public class PisteMockTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        when(mockPiste.getnumPiste()).thenReturn(2L);
-        when(mockPiste.getcouleur()).thenReturn(Couleur.BLEU);
-        when(mockPiste.getlongeur()).thenReturn(100);
-        when(mockPiste.getpente()).thenReturn(30);
+        when(mockPiste.getNumPiste()).thenReturn(2L);
+        when(mockPiste.getCouleur()).thenReturn(Couleur.BLEU);
+        when(mockPiste.getLongeur()).thenReturn(100);
+        when(mockPiste.getPente()).thenReturn(30);
     }
 
     @Test
     void testGetNumPisteWithMock() {
-        assertEquals(2L, mockPiste.getnumPiste());
+        assertEquals(2L, mockPiste.getNumPiste());
     }
 
    
     @Test
     void testGetColorWithMock() {
-        assertEquals(Couleur.BLEU, mockPiste.getcouleur());
+        assertEquals(Couleur.BLEU, mockPiste.getCouleur());
     }
 
     @Test
     void testGetLengthWithMock() {
-        assertEquals(100, mockPiste.getlongeur());
+        assertEquals(100, mockPiste.getLongeur());
     }
 
     @Test
     void testGetSlopeWithMock() {
-        assertEquals(30, mockPiste.getpente());
+        assertEquals(30, mockPiste.getPente());
     }
 }
